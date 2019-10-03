0118e6ffa966da9ea3ca76a667d7581714e944d9
cja5 <55702037+cja5@users.noreply.github.com>
2019-10-03 15:09:12 +0100

    Arrow keys + Action Happens
     Made it so if you press space then it changes colour temporarily, this simulates sword movement.

--- onlineDemo.js
+++ onlineDemo.js
@@ -1,7 +1,7 @@
-//source: https://www.includehelp.com/code-snippets/move-object-with-arrow-keys-using-javascript-function.aspx
 
   //init object globally
-  var objImage= null;
+  var objImage = null;
+
   function init(){
     objImage=document.getElementById("animate");
     objImage.style.position='relative';
@@ -9,10 +9,9 @@
     objImage.style.top='0px';
   }
 
-
-  function getKeyAndMove(e){
-    var key_code=e.which||e.keyCode;
-    switch(key_code){
+//detects key press from html and checks which key
+document.onkeydown = function(e) {
+    switch(e.keyCode){
       case 37: //left arrow key
         moveLeft();
         break;
@@ -25,20 +24,32 @@
       case 40: //down arrow key
         moveDown();
         break;
+      case 32: //space key
+        change();
+
     }
   }
+
+  //number changes speed
   function moveLeft(){
-    objImage.style.left=parseInt(objImage.style.left)-5 +'px';
+    objImage.style.left=parseInt(objImage.style.left)-7.5 +'px';
   }
   function moveUp(){
-    objImage.style.top=parseInt(objImage.style.top)-5 +'px';
+    objImage.style.top=parseInt(objImage.style.top)-7.5 +'px';
   }
   function moveRight(){
-    objImage.style.left=parseInt(objImage.style.left)+5 +'px';
+    objImage.style.left=parseInt(objImage.style.left)+7.5+'px';
   }
   function moveDown(){
-    objImage.style.top=parseInt(objImage.style.top)+5 +'px';
+    objImage.style.top=parseInt(objImage.style.top)+7.5 +'px';
   }
-  window.onload=init;
+  function change(){
+    //changes box colour
+    objImage.style.background = "blue";
+    //waits for 500ms then changes back into original colour
+    setTimeout(function(){objImage.style.background = "red"}, 500);
+  }
 
-  document.onkeydown = getKeyAndMove(event);
+
+  //as soon as window is loaded will initialise
+  window.onload=init;
